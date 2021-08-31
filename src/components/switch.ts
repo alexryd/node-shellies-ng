ef68c5fa9f1b5534723f78cc4df17fad887dfd7f
Alexander Ryden <alexander.ryden@gmail.com>
2021-08-31 12:16:53 +0200

    Add the Switch component

---
+++ src/components/switch.ts
@@ -0,0 +1,78 @@
+import { characteristic, Component } from './base';
+
+export interface SwitchEnergyCounterAttributes {
+  total: number;
+  by_minute: number[];
+  minute_ts: number;
+}
+
+export interface SwitchTemperatureAttributes {
+  tC: number | null;
+  tF: number | null;
+}
+
+export interface SwitchAttributes {
+  id: number;
+  source: string;
+  output: boolean;
+  timer_started_at: number;
+  timer_duration: number;
+  apower: number;
+  voltage: number;
+  aenergy: SwitchEnergyCounterAttributes;
+  temperature: SwitchTemperatureAttributes;
+}
+
+/**
+ * Represents a switch (relay) of a device.
+ */
+export class Switch extends Component {
+  /**
+   * true if the output channel is currently on, false otherwise.
+   */
+  @characteristic()
+  readonly output: boolean = false;
+
+  /**
+   * The current (last measured) instantaneous power delivered to the attached
+   * load (if applicable).
+   */
+  @characteristic()
+  readonly apower: number = 0;
+
+  /**
+   * Current voltage (if applicable).
+   */
+  @characteristic()
+  readonly voltage: number = 0;
+
+  /**
+   * Information about the energy counter (if applicable).
+   */
+  @characteristic()
+  readonly aenergy: SwitchEnergyCounterAttributes = {
+    total: 0,
+    by_minute: [],
+    minute_ts: 0,
+  };
+
+  /**
+   * Information about the temperature.
+   */
+  @characteristic()
+  readonly temperature: SwitchTemperatureAttributes = {
+    tC: null,
+    tF: null,
+  };
+
+  /**
+   * @param id - ID of this Switch component.
+   */
+  constructor(readonly id: number = 0) {
+    super();
+  }
+
+  update(data: Partial<SwitchAttributes>) {
+    super.update(data);
+  }
+}
