7dd13d6077211e4ccdc9c37c78114bde4f7c4d45
Alexander Ryden <alexander.ryden@gmail.com>
2021-12-11 22:36:45 +0100

    Add the HTTP.POST method

--- src/services/http.ts
+++ src/services/http.ts
@@ -1,7 +1,7 @@
 import { Device } from '../devices';
 import { Service } from './base';
 
-export interface HttpGetResponse {
+export interface HttpResponse {
   code: number;
   message: string;
   headers: {[k: string]: string};
@@ -24,9 +24,28 @@ export class HttpService extends Service {
    * @param ssl_ca - The certificate authority to use for HTTPS requests.
    */
   get(url: string, timeout?: number,
-    ssl_ca?: '*' | 'user_ca.pem' | '' | null): PromiseLike<HttpGetResponse> {
-    return this.rpc<HttpGetResponse>('GET', {
+    ssl_ca?: '*' | 'user_ca.pem' | '' | null): PromiseLike<HttpResponse> {
+    return this.rpc<HttpResponse>('GET', {
       url,
+      timeout,
+      ssl_ca,
+    });
+  }
+
+  /**
+   * Sends an HTTP POST request.
+   * @param url - The URL to send the request to.
+   * @param body - The request body.
+   * @param content_type - The type of content being sent.
+   * @param timeout - Timeout, in seconds.
+   * @param ssl_ca - The certificate authority to use for HTTPS requests.
+   */
+  post(url: string, body: string, content_type = 'application/json', timeout?: number,
+    ssl_ca?: '*' | 'user_ca.pem' | '' | null): PromiseLike<HttpResponse> {
+    return this.rpc<HttpResponse>('POST', {
+      url,
+      body,
+      content_type,
       timeout,
       ssl_ca,
     });
