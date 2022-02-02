be1c7864b76321cbbcf5ed69ddd851dc9a238406
Alexander Ryden <alexander.ryden@gmail.com>
2022-02-02 21:57:14 +0100

    Add support for the Shelly Pro 1

---
+++ src/devices/shelly-pro-1.ts
@@ -0,0 +1,37 @@
+import { component, Device } from './base';
+import {
+  BluetoothLowEnergy,
+  Cloud,
+  Input,
+  Mqtt,
+  Switch,
+  WiFi,
+} from '../components';
+
+export class ShellyPro1 extends Device {
+  static readonly model = 'SPSW-001XE16EU';
+  static readonly modelName = 'Shelly Pro 1';
+
+  @component()
+  readonly wifi = new WiFi(this);
+
+  @component('ble')
+  readonly bluetoothLowEnergy = new BluetoothLowEnergy(this);
+
+  @component()
+  readonly cloud = new Cloud(this);
+
+  @component()
+  readonly mqtt = new Mqtt(this);
+
+  @component('input:0')
+  readonly input0 = new Input(this, 0);
+
+  @component('input:1')
+  readonly input1 = new Input(this, 1);
+
+  @component('switch:0')
+  readonly switch0 = new Switch(this, 0);
+}
+
+Device.registerClass(ShellyPro1);
