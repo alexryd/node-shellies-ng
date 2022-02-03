3ee0efac3b6d46c41f6e1ee23bbc17e8985a2e82
Alexander Ryden <alexander.ryden@gmail.com>
2022-02-03 06:40:42 +0100

    Add support for the Shelly Pro 2 PM

---
+++ src/devices/shelly-pro-2-pm.ts
@@ -0,0 +1,40 @@
+import { component, Device } from './base';
+import {
+  BluetoothLowEnergy,
+  Cloud,
+  Input,
+  Mqtt,
+  Switch,
+  WiFi,
+} from '../components';
+
+export class ShellyPro2Pm extends Device {
+  static readonly model = 'SPSW-002PE16EU';
+  static readonly modelName = 'Shelly Pro 2 PM';
+
+  @component()
+  readonly wifi = new WiFi(this);
+
+  @component('ble')
+  readonly bluetoothLowEnergy = new BluetoothLowEnergy(this);
+
+  @component()
+  readonly cloud = new Cloud(this);
+
+  @component()
+  readonly mqtt = new Mqtt(this);
+
+  @component('input:0')
+  readonly input0 = new Input(this, 0);
+
+  @component('input:1')
+  readonly input1 = new Input(this, 1);
+
+  @component('switch:0')
+  readonly switch0 = new Switch(this, 0);
+
+  @component('switch:1')
+  readonly switch1 = new Switch(this, 1);
+}
+
+Device.registerClass(ShellyPro2Pm);
