0bc5e04a24933735471cb3b8e2d236f093cf975a
Nick Moone <33722387+nickmoone@users.noreply.github.com>
2024-02-20 22:47:10 +0100

    Create shelly-plus-i4dc.ts.

---
+++ src/devices/shelly-plus-i4dc.ts
@@ -0,0 +1,47 @@
+import { component, Device } from './base';
+import {
+  BluetoothLowEnergy,
+  Cloud,
+  Input,
+  Mqtt,
+  OutboundWebSocket,
+  Script,
+  WiFi,
+} from '../components';
+
+export class ShellyPlusI4 extends Device {
+  static readonly model: string = 'SNSN-0D24X';
+  static readonly modelName: string = 'Shelly Plus I4DC';
+
+  @component
+  readonly wifi = new WiFi(this);
+
+  @component
+  readonly bluetoothLowEnergy = new BluetoothLowEnergy(this);
+
+  @component
+  readonly cloud = new Cloud(this);
+
+  @component
+  readonly mqtt = new Mqtt(this);
+
+  @component
+  readonly outboundWebSocket = new OutboundWebSocket(this);
+
+  @component
+  readonly input0 = new Input(this, 0);
+
+  @component
+  readonly input1 = new Input(this, 1);
+
+  @component
+  readonly input2 = new Input(this, 2);
+
+  @component
+  readonly input3 = new Input(this, 3);
+
+  @component
+  readonly script = new Script(this);
+}
+
+Device.registerClass(ShellyPlusI4DC);
